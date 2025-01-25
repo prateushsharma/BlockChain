bf0bbba5dde035d0306637bed05b702ba5a4f53d
PRATEUSH SHARMA <115470025+prateushsharma@users.noreply.github.com>
2025-01-25 13:29:36 +0530

    Update ipfs.py
    Changed url

--- ipfs.py
+++ ipfs.py
@@ -2,7 +2,7 @@ import requests
 
 class IPFSManager:
     def __init__(self, pinata_api_key, pinata_api_secret):
-        self.api_url = "https://api.pinata.cloud/"
+        self.api_url = "https://api.pinata.cloud/pinning/pinFileToIPFS/"
         self.headers = {
             "pinata_api_key": "8dc26f5d53e5b5584661", # put ur pinata api key here
             "pinata_secret_api_key": "1581002614b253fbc6f38469ae1426c3fbb84c34df2da7319ca0ef9026030e43", # api secret key as well
